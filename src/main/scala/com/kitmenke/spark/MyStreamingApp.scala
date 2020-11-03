82634ffc291288b3508e7b7f502be310911ae645
RLD <rebeccadohrman@gmail.com>
2020-11-02 21:25:14 -0600

    first batch changes

--- src/main/scala/com/kitmenke/spark/MyStreamingApp.scala
+++ src/main/scala/com/kitmenke/spark/MyStreamingApp.scala
@@ -1,5 +1,8 @@
 package com.kitmenke.spark
 
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Get}
+import org.apache.hadoop.hbase.util.Bytes
 import org.apache.log4j.Logger
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
@@ -15,11 +18,15 @@ object MyStreamingApp {
   lazy val logger: Logger = Logger.getLogger(this.getClass)
   val jobName = "MyStreamingApp"
   // TODO: define the schema for parsing data from Kafka
-  val schema: StructType = ???
+  // val schema: StructType = ???
 
   def main(args: Array[String]): Unit = {
     try {
-      val spark = SparkSession.builder().appName(jobName).master("local[*]").getOrCreate()
+      val spark = SparkSession.builder().appName(jobName).master("local[*]")
+        .config("spark.hadoop.dfs.client.use.datanode.hostname", "true")
+        .config("spark.hadoop.fs.defaultFS", "hdfs://cdh.kitmenke.com:8020")
+        .getOrCreate()
+
       val bootstrapServers = args(0)
       val df = spark
         .readStream
@@ -31,12 +38,26 @@ object MyStreamingApp {
 
       df.printSchema()
 
-      val out = compute(df)
+      val out = compute(df).mapPartitions(partition=>{
+        var connection: Connection = null
+        val conf = HBaseConfiguration.create()
+          conf.set("hbase.zookeeper.quorum", "cdh.kitmenke.com:2181")
+          connection = ConnectionFactory.createConnection(conf)
+          partition.map(row=>{
+            val table = connection.getTable(TableName.valueOf("table-name"))
+            val get = new Get(Bytes.toBytes(row.getAs[String]("customer_id")))
+            //get the row from HBase
+            //get the name and mail from result
+            //return new row with review+name and mail
+        })
+      })
 
       val query = out.writeStream
-        .outputMode(OutputMode.Complete())
+        .outputMode(OutputMode.Append())
         .format("console")
         .trigger(Trigger.ProcessingTime("5 seconds"))
+        .option("truncate", false)
+        .option("checkpointLocation", "/user/rebeccadohrman/checkpoint")
         .start()
 
       query.awaitTermination()
@@ -46,7 +67,11 @@ object MyStreamingApp {
   }
 
   def compute(df: DataFrame): DataFrame = {
-    // TODO: implement your logic here
-    df
+    df.selectExpr("split(value, '\t') as split_value")
+      .selectExpr("split_value[0] as marketplace", "split_value[1] as customer_id", "split_value[2] as review_id",
+        "split_value[3] as product_id", "split_value[4] as product_parent", "split_value[5] as product_title",
+        "split_value[6] as product_category", "split_value[7] as star_rating", "split_value[8] as helpful_votes",
+        "split_value[9] as total_votes", "split_value[10] as vine", "split_value[11] as verified_purchase",
+        "split_value[12] as review_headline", "split_value[13] as review_body", "split_value[14] as review_date")
   }
 }
--- src/test/scala/com/kitmenke/spark/MyStreamingAppTests.scala
+++ src/test/scala/com/kitmenke/spark/MyStreamingAppTests.scala
@@ -10,6 +10,23 @@ import org.scalatest.FunSuite
 class MyStreamingAppTests extends FunSuite with DataFrameSuiteBase {
   import sqlContext.implicits._
 
+  test("should parse reviews data using spark sql") {
+    // create some sample data to run through our program
+    val input = sc.parallelize(
+      List[String](
+        "US\t15959635\tR7BUC5CTHROVD\tB00DOFSWV2\t876290286\tMy Little Pony 6.5 inch Plush Dolls\tToys\t5\t0\t0\tN\tY\tcute cute cute\tI ordered several of these to make a set for my daughter and she loves them! They are in great condition and arrived timely!! Thank you!\t2014-04-23",
+        "US\t42410103\tR2VPFRIVNI8SQC\tB004RC51WY\t265592844\tSwimways Dive 'N Glide Shark\tToys\t1\t0\t0\tN\tY\tOne Star\tShark didn't do what was expected to do as described on advertisement.\t2015-08-16",
+        "US\t39864878\tR2EFHJ8MKL5RFS\tB00W84WCVG\t323121419\tMelissa & Doug Reusable Sticker Pad - Habitats\tToys\t5\t1\t1\tN\tN\tGreat Pads for Story Telling with Stickers\tWe like Melissa and Doug toys for the kids because they all seem to last and have some educational element. These sticker pads are no exception -- I like them most because they encourage the little ones to create narratives and stories.\t2015-08-31"
+      )).toDF("value")
+    input.printSchema()
+    input.show()
+
+    //val result = input.select(sql.functions.split(input("value"), "\t"))
+    val result = MyStreamingApp.compute(input)
+    result.printSchema()
+    result.show(truncate = false)
+
+  }
   test("should parse json") {
     // create some sample data to run through our program
     val input = sc.parallelize(
